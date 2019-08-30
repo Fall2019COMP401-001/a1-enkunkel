5b5d4daa83ee268e2dc0a56b3ca20716cdc2856c
emmakunkel <emmakunkel@152.23.10.168>
2019-08-30 02:12:26 -0400

    wrote novice prelim. commented out example problem.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,37 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		Scanner keyboard = new Scanner(System.in);
+		
+		int people = keyboard.nextInt();
+		String[] names = new String[people];
+		double [] totals = new double[people];
+		//System.out.print(people);// CHECKED
+		
+		for (int p = 0; p < people; p++) {
+			
+			String first_name = keyboard.next();
+			String last_name = keyboard.next();
+			names[p] = first_name.substring(0,1) + ". " + last_name;
+			//System.out.print(names[p]); //CHECKED
+			
+			int num_items = keyboard.nextInt();
+			//System.out.print(num_items); //CHECKED
+			
+			for (int i = 0; i < num_items; i++) {
+				double num_single_items = keyboard.nextDouble();
+				String irrelevant = keyboard.next(); //Irrelevant
+				double price = keyboard.nextDouble();
+				
+				
+				totals[p] = totals[p] + (num_single_items * price);
+				
+				
+			}
+			System.out.print(names[p] + ": " + String.format("%.2f", totals[p])+"\n");
+		}
+		keyboard.close();
+		
 		
 	}
 }
