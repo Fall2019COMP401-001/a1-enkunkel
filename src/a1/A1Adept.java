a68793fd5ce7f76ab6a69b1bfb7693744e2bd046
emmakunkel <emmakunkel@152.23.10.168>
2019-09-02 02:36:05 -0400

    Created prelim working version of novice

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,6 +1,7 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class A1Adept {
 
@@ -8,7 +9,59 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int numItems = scan.nextInt(); 
+		HashMap<String, Double> items = new HashMap<String, Double>();
 		
+		for(int i = 0; i < numItems; i++) {
+			String tempItemName = scan.next();
+			double tempItemPrice = scan.nextDouble();
+			items.put(tempItemName , tempItemPrice);
+		}
+		
+		int numPeople = scan.nextInt();
+		//System.out.print("FUUUUUUUUUUUCK"); CHECKED
+		
+		String biggestBuyer = "";
+		Double biggestTotal = 0.0;
+		
+		String smallestBuyer = "";
+		Double smallestTotal = 1000000000.0;
+		
+		Double cumulativeTotal = 0.0;
+		
+		for (int i = 0; i < numPeople; i++) {
+			String tempFirstName = scan.next();
+			String tempLastName = scan.next();
+			String tempName = tempFirstName + " " +  tempLastName;
+			
+			int itemsPurchased = scan.nextInt();
+				
+			double personalTotal = 0.0;
+			
+			for (int t = 0; t < itemsPurchased; t++) {
+				int numberItemPurchased = scan.nextInt();
+				personalTotal = personalTotal + (items.get(scan.next()) * numberItemPurchased );			
+			}
+			
+			if (personalTotal > biggestTotal) {
+				biggestBuyer = tempName;
+				biggestTotal = personalTotal;
+			}
+			
+			if (personalTotal < smallestTotal) {
+				smallestBuyer = tempName;
+				smallestTotal = personalTotal;
+			}
+			
+			cumulativeTotal = cumulativeTotal + personalTotal;					
+		}
+		
+		scan.close();
+		
+		System.out.print("Biggest: " + biggestBuyer + " " + "(" + String.format("%.2f", biggestTotal) + ")" + "\n");
+		System.out.print("Smallest: " + smallestBuyer + " " + "(" + String.format("%.2f", smallestTotal) + ")" + "\n");
+		System.out.print("Average: " + String.format("%.2f", (cumulativeTotal / numPeople)));
+	
+		//String.format("%.2f", x)
 	}
 }
